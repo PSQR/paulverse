4911a7960ee7209c1d3064e2afd98fccb05c1429
Paul.Perrin <pperrin@grrapiap853.na.steelcase.net>
2020-01-28 14:41:04 -0500

    updates

--- R/Production_Ready_Functions.R
+++ R/Production_Ready_Functions.R
@@ -27,3 +27,43 @@ rs_by_var <- function(df, byvars, num){
   return(temp_3)
   
 }
+
+#' cal_to_fisc_per
+
+cal_to_fisc_per <- function(){
+  
+  load(file = "/opt/R/R_project/pperrin/2020/SOP_Rebuild_w_Danny/Input_Data/inf_dt.Rda")
+  
+  current_day <- as.data.frame(subset(inf_dt, as.Date(inf_dt$CLDR_DT) == Sys.Date()))
+  
+  return(gsub("-", "", current_day$FCL_YR_PER_C))
+  
+}
+
+#' cal.to.fisc.wk
+
+cal_to_fisc_wk <- function(){
+  
+  load(file = "/opt/R/R_project/pperrin/2020/SOP_Rebuild_w_Danny/Input_Data/inf_dt.Rda")
+  
+  current_day <- as.data.frame(subset(inf_dt, as.Date(inf_dt$CLDR_DT) == Sys.Date()))
+  
+  return(current_day$FCL_WK_BGN_DT)
+  
+}
+
+#' save.time.stamped
+
+save_time_stamped <- function(df, the_file_path, the_time_stamp){
+  
+  save(df, file = the_file_path)
+  
+  the_list <- unlist(strsplit(the_file_path, "\\."))
+  
+  directory <- the_list[1]
+  
+  extension <- the_list[2]
+  
+  save(df, file = paste0(directory, "_", the_time_stamp, "." , extension))
+  
+}
